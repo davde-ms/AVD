995e131612b9c3683d3bd6b19657a00546b99b56
David De Backer <davde@microsoft.com>
2023-10-11 13:39:00 -0400

    modified script

--- Refresh Hosts/Scripts/packageInstaller.ps1
+++ Refresh Hosts/Scripts/packageInstaller.ps1
@@ -8,10 +8,7 @@
 
 Param (        
     [Parameter(Mandatory=$true)]
-        [string]$FSLogixInstaller,
-
-    [Parameter(Mandatory=$false)]
-        [string]$VHDSize
+        [string]$packageZipNameURI
 )
 
 ########################
@@ -19,64 +16,63 @@ Param (
 ########################
 $localDownloadPath            = "c:\temp\install\"
 $packageZipName             = 'package.zip'
-$templateFilePathFolder = "C:\AVDImage"
+
 
 ####################################
 #    Test/Create Temp Directory    #
 ####################################
 if((Test-Path c:\temp) -eq $false) {
-    Write-Host "AVD AIB Customization - Install FSLogix : Creating temp directory"
+    Write-Host "Package installation : Creating temp directory"
     New-Item -Path c:\temp -ItemType Directory
 }
 else {
-    Write-Host "AVD AIB Customization - Install FSLogix : C:\temp already exists"
+    Write-Host "Package installation : C:\temp already exists"
 }
-if((Test-Path $LocalWVDpath) -eq $false) {
-    Write-Host "AVD AIB Customization - Install FSLogix : Creating directory: $LocalWVDpath"
-    New-Item -Path $LocalWVDpath -ItemType Directory
+if((Test-Path $localDownloadPath) -eq $false) {
+    Write-Host "Package installation  : Creating directory: $localDownloadPath "
+    New-Item -Path $localDownloadPath -ItemType Directory
 }
 else {
-    Write-Host "AVD AIB Customization - Install FSLogix : $LocalWVDpath already exists"
+    Write-Host "Package installation : $localDownloadPath already exists"
 }
 
 #################################
-#    Download WVD Componants    #
+#    Download Package           #
 #################################
-Write-Host "AVD AIB Customization - Install FSLogix : Downloading FSLogix from URI: $FSLogixInstaller"
-Invoke-WebRequest -Uri $FSLogixInstaller -OutFile "$LocalWVDpath$FSInstaller"
+Write-Host "Package installation : Downloading package file from URI: $packageZipName"
+Invoke-WebRequest -Uri $packageZipNameURI -OutFile "$localDownloadPath$packageZipName"
 
 
 ##############################
-#    Prep for WVD Install    #
+#    Unzip package           #
 ##############################
-Write-Host "AVD AIB Customization - Install FSLogix : Unzipping FSLogix installer"
+Write-Host "Package installation : Unzipping package installer"
 Expand-Archive `
-    -LiteralPath "C:\temp\wvd\$FSInstaller" `
-    -DestinationPath "$LocalWVDpath\FSLogix" `
+    -LiteralPath "$localDownloadPath$packageZipName" `
+    -DestinationPath "$localDownloadPath\$packageZipName.Substring(0, $filename.LastIndexOf('.'))" `
     -Force `
     -Verbose
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Set-Location $LocalWVDpath 
-Write-Host "AVD AIB Customization - Install FSLogix : UnZip of FSLogix complete"
+Set-Location $localDownloadPath 
+Write-Host "Package installation : UnZip of $packageZipName.Substring(0, $filename.LastIndexOf('.')) complete"
 
 
 #########################
-#    FSLogix Install    #
+#    Packge Install     #
 #########################
-Write-Host "AVD AIB Customization - Install FSLogix : Starting to install FSLogix"
-$fslogix_deploy_status = Start-Process `
-    -FilePath "$LocalWVDpath\FSLogix\x64\Release\FSLogixAppsSetup.exe" `
+Write-Host "Package installation : Starting to install $packageZipName.Substring(0, $filename.LastIndexOf('.'))"
+$package_deploy_status = Start-Process `
+    -FilePath "$localDownloadPath\$packageZipName.Substring(0, $filename.LastIndexOf('.'))\fullpath_to_installer" `
     -ArgumentList "/install /quiet /norestart" `
     -Wait `
     -Passthru
 
-if(!($PSBoundParameters.ContainsKey('VHDSize'))) {
-    $VHDSize = "30000"
-}
-#######################################
-#    FSLogix User Profile Settings    #
-#######################################
-Write-Host "AVD AIB Customization - Install FSLogix : Configure FSLogix Profile Settings"
+
+###########################
+#    Registry Settings    #
+###########################
+
+<# Write-Host "Package installation : Apply registry Settings, uncomment and use as example"
 Push-Location 
 Set-Location HKLM:\SOFTWARE\
 New-Item `
@@ -94,95 +90,18 @@ Set-ItemProperty `
     -Type "Dword" `
     -Value "1"
 
-if(($PSBoundParameters.ContainsKey('ProfilePath'))) {
-    New-ItemProperty `
-    -Path HKLM:\Software\FSLogix\Profiles `
-    -Name "CCDLocations" `
-    -Value "type=smb,connectionString=$ProfilePath" `
-    -PropertyType MultiString `
-    -Force
-}
-
-Set-ItemProperty `
-    -Path HKLM:\Software\FSLogix\Profiles `
-    -Name "SizeInMBs" `
-    -Type "Dword" `
-    -Value $VHDSize
-Set-ItemProperty `
-    -Path HKLM:\Software\FSLogix\Profiles `
-    -Name "IsDynamic" `
-    -Type "Dword" `
-    -Value "1"
-Set-ItemProperty `
-    -Path HKLM:\Software\FSLogix\Profiles `
-    -Name "VolumeType" `
-    -Type String `
-    -Value "vhdx"
-Set-ItemProperty `
-    -Path HKLM:\Software\FSLogix\Profiles `
-    -Name "FlipFlopProfileDirectoryName" `
-    -Type "Dword" `
-    -Value "1" 
 Set-ItemProperty `
     -Path HKLM:\Software\FSLogix\Profiles `
     -Name "SIDDirNamePattern" `
     -Type String `
-    -Value "%username%%sid%"
-Set-ItemProperty `
-    -Path HKLM:\Software\FSLogix\Profiles `
-    -Name "SIDDirNameMatch" `
-    -Type String `
-    -Value "%username%%sid%"
-Set-ItemProperty `
-    -Path HKLM:\Software\FSLogix\Profiles `
-    -Name DeleteLocalProfileWhenVHDShouldApply `
-    -Type DWord `
-    -Value 1
+    -Value "%username%%sid%" #>
 
-#Reference: https://learn.microsoft.com/en-us/azure/architecture/example-scenario/wvd/windows-virtual-desktop-fslogix#add-exclusions-for-microsoft-defender-for-cloud-by-using-powershell
-Write-Host "AVD AIB Customization - Install FSLogix : Adding exclusions for Microsoft Defender"
-
-try {
-     $filelist = `
-  "%ProgramFiles%\FSLogix\Apps\frxdrv.sys", `
-  "%ProgramFiles%\FSLogix\Apps\frxdrvvt.sys", `
-  "%ProgramFiles%\FSLogix\Apps\frxccd.sys", `
-  "%TEMP%\*.VHD", `
-  "%TEMP%\*.VHDX", `
-  "%Windir%\TEMP\*.VHD", `
-  "%Windir%\TEMP\*.VHDX" `
-
-    $processlist = `
-    "%ProgramFiles%\FSLogix\Apps\frxccd.exe", `
-    "%ProgramFiles%\FSLogix\Apps\frxccds.exe", `
-    "%ProgramFiles%\FSLogix\Apps\frxsvc.exe"
-
-    Foreach($item in $filelist){
-        Add-MpPreference -ExclusionPath $item}
-    Foreach($item in $processlist){
-        Add-MpPreference -ExclusionProcess $item}
-
-
-    Add-MpPreference -ExclusionPath "%ProgramData%\FSLogix\Cache\*.VHD"
-    Add-MpPreference -ExclusionPath "%ProgramData%\FSLogix\Cache\*.VHDX"
-    Add-MpPreference -ExclusionPath "%ProgramData%\FSLogix\Proxy\*.VHD"
-    Add-MpPreference -ExclusionPath "%ProgramData%\FSLogix\Proxy\*.VHDX"
-}
-catch {
-     Write-Host "AVD AIB Customization - Install FSLogix : Exception occurred while adding exclusions for Microsoft Defender"
-     Write-Host $PSItem.Exception
-}
-
-Write-Host "AVD AIB Customization - Install FSLogix : Finished adding exclusions for Microsoft Defender"
 
 #Cleanup
-if ((Test-Path -Path $templateFilePathFolder -ErrorAction SilentlyContinue)) {
-    Remove-Item -Path $templateFilePathFolder -Force -Recurse -ErrorAction Continue
+if ((Test-Path -Path $localDownloadPath -ErrorAction SilentlyContinue)) {
+    Remove-Item -Path $localDownloadPath -Force -Recurse -ErrorAction Continue
 }
 
-if ((Test-Path -Path $LocalWVDpath -ErrorAction SilentlyContinue)) {
-    Remove-Item -Path $LocalWVDpath -Force -Recurse -ErrorAction Continue
-}
 
 #############
 #    END    #
