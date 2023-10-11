4c17f3cd6396864ef012064e754d171a128bce60
David De Backer <davde@microsoft.com>
2023-10-11 16:51:09 -0400

    moved tls setting

--- Refresh Hosts/Scripts/packageInstaller.ps1
+++ Refresh Hosts/Scripts/packageInstaller.ps1
@@ -1,10 +1,10 @@
 <#Author       : David De Backer (based on a script by Akash Chawla)
-# Usage        : Install and software paclage ABC
+# Usage        : Install and software package ABC
 #>
 
-#######################################
-#     Install ABC                     #
-#######################################
+###############
+# Install ABC #
+###############
 
 Param (        
     [Parameter(Mandatory=$true)]
@@ -18,9 +18,9 @@ $localDownloadPath            = "c:\temp\install\"
 $packageZipName             = 'package.zip'
 
 
-####################################
-#    Test/Create Temp Directory    #
-####################################
+##############################
+# Test/Create Temp Directory #
+##############################
 if((Test-Path c:\temp) -eq $false) {
     Write-Host "Package installation : Creating temp directory"
     New-Item -Path c:\temp -ItemType Directory
@@ -36,41 +36,42 @@ else {
     Write-Host "Package installation : $localDownloadPath already exists"
 }
 
-#################################
-#    Download Package           #
-#################################
+####################
+# Download Package #
+####################
 Write-Host "Package installation : Downloading package file from URI: $packageZipName"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri $packageZipNameURI -OutFile "$localDownloadPath$packageZipName"
 
 
-##############################
-#    Unzip package           #
-##############################
+#################
+# Unzip package #
+#################
 Write-Host "Package installation : Unzipping package installer"
 Expand-Archive `
     -LiteralPath "$localDownloadPath$packageZipName" `
     -DestinationPath "$localDownloadPath\$packageZipName.Substring(0, $filename.LastIndexOf('.'))" `
     -Force `
     -Verbose
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 Set-Location $localDownloadPath 
 Write-Host "Package installation : UnZip of $packageZipName.Substring(0, $filename.LastIndexOf('.')) complete"
 
 
-#########################
-#    Packge Install     #
-#########################
+###################
+# Package Install #
+###################
 Write-Host "Package installation : Starting to install $packageZipName.Substring(0, $filename.LastIndexOf('.'))"
 $package_deploy_status = Start-Process `
-    -FilePath "$localDownloadPath\$packageZipName.Substring(0, $filename.LastIndexOf('.'))\fullpath_to_installer" `
+    -FilePath "$localDownloadPath\$packageZipName.Substring(0, $filename.LastIndexOf('.'))\fullpath_to_installer.exe_or_msi" `
     -ArgumentList "/install /quiet /norestart" `
     -Wait `
     -Passthru
 
 
-###########################
-#    Registry Settings    #
-###########################
+#####################
+# Registry Settings #
+#####################
 
 <# Write-Host "Package installation : Apply registry Settings, uncomment and use as example"
 Push-Location 
